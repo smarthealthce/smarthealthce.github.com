968b7284efee87ec3bf9f8dbc5e4a952fa0f3b42
Matheus Paixao <matheus.tec3@gmail.com>
2018-08-25 11:34:56 -0300

    -> Add seção about-us-pt1 com video

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
     // $(function(){
     //     $('#Container').mixItUp();
     // });
-    var mixer = mixitup('#filter-content');
+    //var mixer = mixitup('#filter-content');
     $(".controls .filter").on('click', function(event){
         $(".controls .filter").removeClass('active');
         $(this).addClass('active');
